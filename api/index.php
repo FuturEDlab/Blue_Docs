8fca040c15ae88401d005c155e8a9ac89af5cba4
yeakeya <yeakeya@mail.gvsu.edu>
2026-02-05 19:54:30 -0500

    Implemented Login and Signup Pages (visual only)

--- api/index.php
+++ api/index.php
@@ -3,15 +3,44 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Blue Docs</title>
+
+		<link href="/styles/output.css" rel="stylesheet" type="text/css">
+		<script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
 	</head>
 
-	<header>
-		<h1>Temporary Index Page!</h1>
-		<button onclick="window.location.href='login.php'">Sign In with Google</button>
-	</header>
-
 	<body>
-		<p>Content</p>
-		<div id="response"></div>
+
+		<!-- Navbar Header -->
+		<header class="sticky w-full h-[20vh] top-0 p-10 nav-header">
+
+			<!-- Logo -->
+			<img src="/public/templogo.svg" alt="Temporary Blue Docs Logo" class="float-left h-full">
+
+			<!-- Account Dropdown -->
+			<el-dropdown class="float-right">
+
+				<!-- Account Picture -->
+				<button>
+					<span class="sr-only">Open Profile Menu</span>
+					<img class="size-8" src="/public/profile.png" alt="Default Account Image">
+				</button>
+
+				<!-- Dropdown Contents -->
+				<el-menu anchor="bottom end" popover>
+					<div class="py-1">
+						<button onclick="window.location.href='login.php';">Sign In to BlueDocs</button>
+					</div>
+				</el-menu>
+
+			</el-dropdown>
+		</header>
+
+		<!-- Main Content -->
+		<main class="relative min-h-screen p-6">
+			<p class="font-bold">Content</p>
+		</main>
 	</body>
+
+	<!-- Dummy script to prevent rendering before loading CSS -->
+	<script>0</script>
 </HTML>
--- api/signup.php
+++ api/signup.php
@@ -2,7 +2,7 @@
 <HTML>
 	<head>
 		<meta charset="UTF-8">
-		<title>Blue Docs Login</title>
+		<title>Blue Docs Sign Up</title>
 
 		<link href="/styles/output.css" rel="stylesheet" type="text/css">
 	</head>
@@ -10,26 +10,30 @@
 	<body>
 		<main class="flex flex-col items-center justify-center min-h-screen">
 
-			<!-- Login Container -->
+			<!-- Signup Container -->
 			<div class="flex flex-col items-center justify-between w-1/5 gap-7">
 
 				<!-- Logo -->
 				<img src="/public/templogo.svg" alt="Temporary Blue Docs Logo" class="w-full">
 
-				<!-- Login Form -->
-				<h1 class="">Log in with an existing account</h1>
+				<!-- Signup Form -->
+				<h1 class="">Sign up with a new account</h1>
 				<form action="#" method="POST" class="flex flex-col items-center justify-between w-full gap-4">
+					<div class="w-full">
+						<input id="firstname" type="text" name="firstname" required autocomplete="firstname" placeholder="First Name" class="block w-47/100 float-left rounded-sm outline-4 px-1"/>
+						<input id="lastinitial" type="text" name="lastinitial" required autocomplete="lastinitial" placeholder="Last Initial" class="block w-47/100 float-right rounded-sm outline-4 px-1"/>
+					</div>
 					<div class="w-full">
 						<input id="email" type="text" name="email" required autocomplete="email" placeholder="Email" class="block w-full rounded-sm outline-4 px-1"/>
 					</div>
 					<div class="w-full">
 						<input id="password" type="password" name="password" required autocomplete="password" placeholder="Password" class="block w-full rounded-sm outline-4 px-1"/>
 					</div>
-					<button type="submit" class="block w-full rounded-sm bg-sky-500 outline-sky-500 outline-3 hover:bg-sky-400 hover:outline-sky-400 focus:bg-sky-500 focus:outline-sky-500">Log In →</button>
+					<button type="submit" class="block w-full rounded-sm bg-sky-500 outline-sky-500 outline-3 hover:bg-sky-400 hover:outline-sky-400 focus:bg-sky-500 focus:outline-sky-500">Sign Up →</button>
 				</form>
 
-				<!-- Signup Link -->
-				<a href="/api/signup.php" class="font-bold">or sign up here!</a>
+				<!-- Login Link -->
+				<a href="/api/login.php" class="font-bold">or log in here!</a>
 
 			</div>
 		</main>
